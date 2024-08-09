5aa717a008ba6c5dc8a301f0500f1fd509f9246c
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2024-08-09 13:31:11 -0500

    added functionality for card class component

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,27 +8,28 @@ const Card = ({ title, description, imageSrc }) => (
     borderRadius="lg"
     overflow="hidden"
     boxShadow="lg"
-    p={4}
+    /* p={4} */
     backgroundColor="white"
   >
     <Image
       src={imageSrc}
       alt={title}
       borderRadius="md"
-      mb={4}
+      mb={5}
     />
-    <VStack align="start" spacing={3}>
+    <VStack align="start" spacing={3} ml={4}>
       <Heading as="h3" size="md" color="gray.800">
         {title}
       </Heading>
       <Text color="gray.600">
         {description}
       </Text>
-      <HStack spacing={2} mt={4}>
-        <Text color="blue.500" fontWeight="bold">
-          Learn More
+      <HStack spacing={2} mt={3} mb={4}>
+        <Text color="black">
+          See More
         </Text>
-        <FontAwesomeIcon icon={faArrowRight} size="sm" />
+        { /* icon is included I dont know why it's not rendering in browser */ }
+        <FontAwesomeIcon icon={faArrowRight} size="1x" />
       </HStack>
     </VStack>
   </Box>
