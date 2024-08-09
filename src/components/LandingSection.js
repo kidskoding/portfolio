5e1f50d8bb9fdbb30f106b95a82d1cb4f56ab51a
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2024-08-09 12:13:59 -0500

    updated landing section page on website

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,21 +1,33 @@
 import React from "react";
-import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import { Avatar, Box, Center, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
-const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+const greeting = "Hello, I am Anirudh Konidala!";
+const bio1 = "An undergrad CS student";
+const bio2 = "at the University of Illinois at Urbana-Champaign!";
 
-// Implement the UI for the LandingSection component according to the instructions.
-// Use a combination of Avatar, Heading and VStack components.
 const LandingSection = () => (
   <FullScreenSection
     justifyContent="center"
     alignItems="center"
     isDarkBackground
-    backgroundColor="#2A4365"
+    backgroundColor="#13294B"
   >
-
+    <VStack spacing={4} align="center">
+      <Avatar
+        size="xl"
+        name="Anirudh Konidala"
+        src="../images/kidskoding.png"
+        mb={-1}
+      />
+      <Heading as="h2" size="xs" color="white">{greeting}</Heading>
+    </VStack>
+    <Box mt={8}>
+      <VStack spacing={2} align="center">
+        <Heading as="h2" size="xl" color="white">{bio1}</Heading>
+        <Heading as="h2" size="xl" color="white">{bio2}</Heading>
+      </VStack>
+    </Box>
   </FullScreenSection>
 );
 
